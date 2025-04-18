e6f5eba1e249b21bc04f04de095d9c0bfa9704f6
Sota-Minokawa <soumino9@gmail.com>
2025-04-18 16:47:18 +0900

    必要最低限の環境構築終了

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int S;
+    cin >> S;
+
+    if (200 <= S && S <= 299)
+    {
+        cout << "Success" << endl;
+    }
+    else
+    {
+        cout << "Failure" << endl;
+    }
+
+    return 0;
+}
