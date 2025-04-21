07703d909308de8764256c2d5593297230284ea1
Sota-Minokawa <soumino9@gmail.com>
2025-04-21 10:44:27 +0900

    テンプレートの追加

---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ull = unsigned long long;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // ここにコードを書く
+
+    return 0;
+}
