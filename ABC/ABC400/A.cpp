1381cbbfc95ab9fcf79c8f08822d1bf472aeb635
Sota-Minokawa <soumino9@gmail.com>
2025-04-24 11:37:33 +0900

    AC

---
+++ ABC/ABC400/A.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define all(x) (x).begin(), (x).end()
+using ll = long long;
+using ull = unsigned long long;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const int MOD = 1000000007;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    // Template
+    int A;
+    cin >> A;
+    if (400 % A != 0)
+        cout << -1;
+    else
+        cout << 400 / A;
+    cout << endl;
+    return 0;
+}
