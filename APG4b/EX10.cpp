2de7c9af3142b22a700d2969166d81a4cde04356
Sota-Minokawa <soumino9@gmail.com>
2025-04-29 19:04:23 +0900

    EX10

---
+++ APG4b/EX10.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); i++)
+
+int main()
+{
+    int A, B;
+    cin >> A >> B;
+
+    // ここにプログラムを追記
+    cout << "A:";
+    rep(i, A)
+    {
+        cout << "]";
+    }
+    cout << endl;
+    cout << "B:";
+    rep(i, B)
+    {
+        cout << "]";
+    }
+    cout << endl;
+    return 0;
+}
